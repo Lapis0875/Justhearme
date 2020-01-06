6aa29afc1b57dab9de7ac58d4b48e60116f4351f
SkyLightQP <combbm@naver.com>
2020-01-06 21:30:49 +0900

    feat: add toolbar

--- app/src/main/java/com/teamif/justhear/MainActivity.kt
+++ app/src/main/java/com/teamif/justhear/MainActivity.kt
@@ -2,11 +2,20 @@ package com.teamif.justhear
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolBar()
     }
+
+    private fun initToolBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
 }
