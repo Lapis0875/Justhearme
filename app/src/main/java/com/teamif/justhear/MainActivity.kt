ec6a69b80dd69384df7f29a721ebf1706444827b
SkyLightQP <combbm@naver.com>
2020-01-06 21:47:16 +0900

    feat: implement bottom navigation bar

--- app/src/main/java/com/teamif/justhear/MainActivity.kt
+++ app/src/main/java/com/teamif/justhear/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.teamif.justhear
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.teamif.justhear.fragments.FragmentTimeline
 
 class MainActivity : AppCompatActivity() {
-    
+    private val fragmentTimeline = FragmentTimeline()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragmentTimeline).commitAllowingStateLoss()
+
         initToolBar()
+        initBottomNavigation()
     }
 
     private fun initToolBar() {
@@ -18,4 +26,17 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
     }
 
+    private fun initBottomNavigation() {
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            val trans = supportFragmentManager.beginTransaction()
+            when (menuItem.itemId) {
+                R.id.nav1 -> trans.replace(R.id.frameLayout, fragmentTimeline).commitAllowingStateLoss()
+                R.id.nav2 -> Toast.makeText(applicationContext, "2", Toast.LENGTH_SHORT).show()
+                R.id.nav3 -> Toast.makeText(applicationContext, "3", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+    }
 }
